0cdc3a2a934c2c5777d409079f4a901b45bc8fdd
moatasem <programmerms17@gmail.com>
2023-09-26 06:17:02 -0700

    add screen shoots

---
+++ lib/login/widgets/my_painter.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class MyPainter extends CustomPainter {
+  final double radius;
+
+  MyPainter(this.radius);
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final paint = Paint()
+      ..shader = const LinearGradient(
+              colors: [Color(0xffFD5E3D), Color(0xffC43990)],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight)
+          .createShader(Rect.fromCircle(
+        center: Offset(0, 0),
+        radius: radius,
+      ));
+
+    canvas.drawCircle(Offset.zero, radius, paint);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return true;
+  }
+}
+
+
+class MyBehavior extends ScrollBehavior {
+  @override
+  Widget buildViewportChrome(
+      BuildContext context, Widget child, AxisDirection axisDirection) {
+    return child;
+  }
+}
